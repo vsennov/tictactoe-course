da8249dd3b48957c843e09af426ee37f9594ae0d
koikovaarina <koikova_arina@mail.ru>
2026-05-17 12:17:57 +0300

    [#2] Реализован класс MyBot

--- include/my_bot.hpp
+++ include/my_bot.hpp
@@ -10,10 +10,20 @@ using game::Point;
 using game::Sign;
 using game::State;
 
+/**
+ * @brief простой бот, делающий случайные легальные ходы
+ */
 class MyBot : public IPlayer {
 public:
+    /**
+     * @brief
+     * @param name
+     */
     explicit MyBot(const char* name = "MyBot");
-
+    
+    /**
+     * @brief
+     */
     ~MyBot() override = default;
 
     void set_sign(Sign sign) override;
@@ -21,9 +31,8 @@ public:
     const char* get_name() const override;
 
 private:
-    std::string m_name; 
+    std::string m_name;
     Sign m_sign;
-
 };
 
 }
