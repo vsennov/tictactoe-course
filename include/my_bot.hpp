99a8a56ae5101755d69ec1c2e089826d0367c5d1
koikovaarina <koikova_arina@mail.ru>
2026-05-17 12:35:36 +0300

    [#3] Реализовано сканирование линий: count_line() в 4 направлениях

--- include/my_bot.hpp
+++ include/my_bot.hpp
@@ -11,19 +11,11 @@ using game::Sign;
 using game::State;
 
 /**
- * @brief простой бот, делающий случайные легальные ходы
+ * @brief бот с эвристической оценкой позиций
  */
 class MyBot : public IPlayer {
 public:
-    /**
-     * @brief
-     * @param name
-     */
     explicit MyBot(const char* name = "MyBot");
-    
-    /**
-     * @brief
-     */
     ~MyBot() override = default;
 
     void set_sign(Sign sign) override;
@@ -31,6 +23,24 @@ public:
     const char* get_name() const override;
 
 private:
+    /**
+     * @brief Считает длину непрерывной цепочки символов в заданном направлении
+     * @param state Состояние игры
+     * @param x Начальная координата X
+     * @param y Начальная координата Y
+     * @param dx Смещение по X (направление)
+     * @param dy Смещение по Y (направление)
+     * @param sign Символ для подсчета
+     * @return Длина цепочки
+     */
+    int count_line(const State& state, int x, int y, int dx, int dy, Sign sign) const;
+    
+    /**
+     * @brief проверяет, находится ли клетка в пределах поля
+     */
+    bool is_in_bounds(const State& state, int x, int y) const;
+
+    
     std::string m_name;
     Sign m_sign;
 };
