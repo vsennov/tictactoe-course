f1c242657e81ebd5b94251f1a60489b267f44543
Buddha <buddcha256@gmail.com>
2026-05-21 12:17:02 +0300

    [#4] добавлены тесты на валидность ходов, победы над базовым уровнем, победы над повышенным уровнем

--- tests/test_stats_vs_harder.cpp
+++ tests/test_stats_vs_harder.cpp
@@ -4,21 +4,22 @@
 #include <iostream>
 #include <cstdlib>
 
-int main(int argc, char *argv[]) {
-    std::cout << "Testing MyPlayer vs baseline easy player\n";
+int main(int argc, char* argv[]) {
+    std::cout << "Testing MyPlayer vs baseline hard player\n";
     if (argc >= 2) {
         std::srand(atoi(argv[1]));
     }
 
-    
+
     ttt::my_player::MyPlayer p1("MyPlayer"); ////поместите вашего игрока сюда
-    ttt::game::IPlayer *p2 = ttt::baseline::get_easy_player("BaselineEasy"); //здесь вы можете выбрать между базовыми игроками: сложным и лёгким
+    //ttt::game::IPlayer* p2 = ttt::baseline::get_easy_player("BaselineHard"); //здесь вы можете выбрать между базовыми игроками: сложным и лёгким
+    ttt::game::IPlayer* p2 = ttt::baseline::get_harder_player("BaselineHard");
 
     auto result = ttt::test::run_game_tests(p1, *p2, 100); //здесь вы можете изменить количество тестовых итераций ~~ 100
-    
-    
-    ttt::test::print_test_results(result, "MyPlayer", "BaselineEasy");
-    
+
+
+    ttt::test::print_test_results(result, "MyPlayer", "BaselineHard");
+
     delete p2;
     return 0;
 }
